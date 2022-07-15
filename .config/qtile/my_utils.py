eff5e2fb83cc733765ea1da9943001847bc60832
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-15 04:43:49 -0300

    removed deprecated file.

--- .config/qtile/my_utils.py
+++
@@ -1,9 +0,0 @@
-def listify(color_name: str, gradient_color: str = "") -> list[str]:
-    """
-    Converts a color into a valid qtile color list format.
-    '#FFFFFF' -> ['#FFFFFF', '#FFFFFF']
-    """
-    if gradient_color:
-        return [color_name, gradient_color]
-
-    return [color_name] * 2
