7dc21e70b3af1d3bd4eb6bc991c14cc72c32ffac
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-16 01:55:31 -0300

    replaced fixed colors for variables.

--- .config/qtile/scripts/dmenu.sh
+++ .config/qtile/scripts/dmenu.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-dmenu_run -i -nb "#191919" -nf "#fea63c" -sb "#fea63c" -sf "#191919" -fn "JetBrainsMono Nerd Font:bold:pixelsize=14"
+normal_background="#282828"
+normal_foreground="#ebdbb2"
+selected_background="#d65d0e"
+selected_foreground="#ebdbb2"
+
+dmenu_run -i -nb $normal_background -nf $normal_foreground -sb $selected_background -sf $selected_foreground -fn "JetBrainsMono Nerd Font:bold:pixelsize=26"
