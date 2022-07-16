ca68cd13922a06b342427e8f8baaecdaec70e37c
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-16 01:59:20 -0300

    separated the arguments by line.

--- .config/qtile/scripts/dmenu.sh
+++ .config/qtile/scripts/dmenu.sh
@@ -5,4 +5,8 @@ normal_foreground="#ebdbb2"
 selected_background="#d65d0e"
 selected_foreground="#ebdbb2"
 
-dmenu_run -i -nb $normal_background -nf $normal_foreground -sb $selected_background -sf $selected_foreground -fn "JetBrainsMono Nerd Font:bold:pixelsize=26"
+dmenu_run -i -nb $normal_background \
+             -nf $normal_foreground \
+             -sb $selected_background \
+             -sf $selected_foreground \
+             -fn "JetBrainsMono Nerd Font:bold:pixelsize=26"
