1a9f0adfba184a79e6337d6100281431b854ba1e
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-17 03:41:35 -0300

    custom theme for change color scheme menu.

--- .config/qtile/scripts/change-color-scheme.sh
+++ .config/qtile/scripts/change-color-scheme.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-color_schemes="dracula\ngruvbox"
-color_scheme=$(printf $color_schemes | rofi -dmenu -i -p " ")
+color_schemes="Dracula\nGruvbox"
+theme_path=$HOME/.config/rofi/change-color-scheme.rasi
+chosen=$(printf $color_schemes | rofi -dmenu -i -p " " -theme $theme_path)
+
+if [[ $chosen =~ "Dracula" ]]; then
+	color_scheme="dracula"
+elif [[ $chosen =~ "Gruvbox" ]]; then
+	color_scheme="gruvbox"
+fi
+
 app_path=$HOME/Documents/python/change-color-scheme/src/main.py
 
 python $app_path $color_scheme
