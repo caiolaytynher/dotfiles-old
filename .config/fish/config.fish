e00368c4b977000da2465933fc342b08240d9692
caiolaytynher <caiolaytynherbr@gmail.com>
2022-11-07 15:48:30 -0300

    added abreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,10 +9,15 @@ end
 alias ls 'exa -lB --no-time --group-directories-first --icons'
 alias la 'exa -laB --no-time --group-directories-first --icons'
 alias cat 'bat'
+alias grep 'rg'
 
 if status --is-interactive
     # source ("/usr/bin/starship" init fish --print-full-init | psub)
     abbr --add --global pyproj "cd $HOME/Documents/python"
+    abbr --add --global cproj "cd $HOME/Documents/c"
+    abbr --add --global prog-ii "cd $HOME/Documents/c/prog-ii"
+    abbr --add --global base "source $HOME/.python-venvs/base/bin/activate.fish"
+    abbr --add --global ic "source $HOME/.python-venvs/ic/bin/activate.fish"
 end
 
 if status --is-interactive && type -q neofetch
